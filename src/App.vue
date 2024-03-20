4155bb990c1c708629c37b6beb017a56a3bc402b
collectivecloudperu <collectivecloudperu@gmail.com>
2024-03-20 00:35:33 -0500

    Cambios

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+
+  // Importamos 'ref'  de Vue 
+  import { ref } from 'vue'
+
+  export default {
+
+    setup() {
+      const agregar = ref(false) // Variable 'agregar' con el atributo 'ref(false)' 
+      return { agregar } // Devolvemos la variable 'agregar' 
+    }
+
+  }
+
+</script>
+
+<template>
+  <h1>Como Agregar Un Nombre De Clase Al Hacer Clic en Vue 3</h1>
+
+  <!-- Al presiona el botón cambiamos o quitamos el nombre clase CSS -->
+  <button @click="agregar = !agregar">Agregar</button> 
+
+  <br>   
+
+  <!-- Imagen a la cual le cambiamos el nombre de clase -->
+  <img :class="{ miclase: agregar }" src="./assets/logo.jpg" width="100px">
+</template>
+
+<style>
+  h1 {
+    font-size: 28px;
+  }
+
+  img {
+    margin-top: 30px;
+  }
+
+  /* Clase CSS */
+  .miclase {
+    border: solid 7px #ffa500;
+  }
+</style>
